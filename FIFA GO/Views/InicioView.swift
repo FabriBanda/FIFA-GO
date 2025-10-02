cf18408bcb78735554a7bc6ce36b651429119128
Fatima Alonso <alonso.fatima03@outlook.com>
2025-10-01 19:13:44 -0600

    Final pantalla inicio

---
+++ FIFA GO/Views/InicioView.swift
@@ -0,0 +1,18 @@
+//
+//  InicioView.swift
+//  FIFA GO
+//
+//  Created by Fatima Alonso on 10/1/25.
+//
+
+import SwiftUI
+
+struct InicioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InicioView()
+}
