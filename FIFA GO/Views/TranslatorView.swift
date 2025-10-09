5a00aab4edd47a660fc864609811af932f219f57
binizaa <binizacruuiz@gmail.com>
2025-10-08 19:07:34 -0600

    Basic structure transcriptor

--- FIFA GO/Views/TranslatorView.swift
+++ FIFA GO/Views/TranslatorView.swift
@@ -1,17 +1,20 @@
-//
-//  TranslatorView.swift
-//  FIFA GO
-//
-//  Created by Biniza Ruiz on 07/10/25.
-//
-
 import SwiftUI
 
 struct TraductorView: View {
-    @StateObject var scanProvider = ScanProvider()
+    @StateObject var scanProvider = ScanProvider()  // Observa el texto publicado desde ScanProvider
+    
     var body: some View {
-        ScanView(scanProvider: scanProvider)
-        //Text("Hola")
+        VStack {
+            ScanView(scanProvider: scanProvider)
+                .frame(height: 600)
+                .cornerRadius(20)
+                .padding()
+            
+            Text(scanProvider.text.isEmpty ? "Escanea algo para empezar..." : scanProvider.text)
+                .font(.headline)
+                .padding()
+                .multilineTextAlignment(.center)
+        }
     }
 }
 
