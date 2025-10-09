6b576c27f75fd957d0cc18c9d15203a4cb29d3a4
FabriBanda <fabribanda438@gmail.com>
2025-10-08 20:35:12 -0600

    ULTIMA VERSION CON JSON COMPLETO

---
+++ FIFA GO/Start/StartView.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  FIFA GO
+//
+//  Created by Fabricio Banda Hernandez on 08/10/25.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartView()
+}
